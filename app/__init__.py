6a3e9adec46d8932b940633bd36f2714302b7f84
Daniella Natani Budiman <87356260+daniella-9@users.noreply.github.com>
2022-11-29 14:40:37 +0700

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,31 +1,23 @@
 import os
-import json
-
 from flask import Flask, render_template
-from .db import ponds, ponds_activation, material,
+from . import db
 
 def create_app(test_config=None):
-    #create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    
     app.config.from_pyfile('settings.cfg', silent=True)
     
-    #ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-        db.init_app(app)
 
-    @app.route('/index')
+    db.init_app(app)
+
     @app.route('/')
     def index():
+        print("test")
         return "Here's the page"
-        
-    @app.get('/')
-    def pond():
-      data = get_ponds({})
-      data = dumps(data)
-      return json.loads(data)
 
 
     return app
