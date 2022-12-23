f39126a50a00ffe379073cfdffaf8a800536b60b
Fahreza <fahrezadhia16@gmail.com>
2022-12-23 20:43:30 +0700

    upload updated chat API

---
+++ mahameru/__init__.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask
+from bson.json_util import dumps
+from . import user
+from . import chat
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    
+    app.config.from_pyfile('settings.cfg', silent=True)
+    app.register_blueprint(user.bp)
+    app.register_blueprint(chat.bp)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def index():
+        print("test")
+        return "Mahameru Chat"
+    
+    return app
---
+++ mahameru/model/db_chat.py
@@ -0,0 +1,102 @@
+import click
+import pymongo
+from flask import current_app, g, request, jsonify
+from flask.cli import with_appcontext
+from bson.son import SON
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+
+def get_db():
+    mongocon = current_app.config['MONGO_CON']
+    dbclient = pymongo.MongoClient(mongocon)
+    g.db = dbclient[current_app.config['DATABASE']]
+    return g.db
+
+def get_collection(colname):
+    if 'db' not in g:
+        get_db()
+    return g.db[colname]
+
+
+def idmatch(data):
+    col = get_collection("user")
+    pipeline = [{"$match": { "nickname" : data }},{"$lookup" : {"from": "chat", "localField": "_id", "foreignField": "from_user", "as": "sent chat" }}]
+    data = col.aggregate(pipeline)
+    return data
+
+def number_match(number):
+    col = get_collection("user")
+    pipeline = [{"$match" : {"notelp" : number}}, {"$lookup" : {"from" : "friend_profile", "localField" : '_id', "foreignField" : 'user_id', "as" : 'friend data'}}]
+    data = col.aggregate(pipeline)
+    data_list = list(data)
+    a = data_list[0]
+    current_app.logger.debug(a)
+    b = a['friend data'][0]
+    current_app.logger.debug(b)
+    if a['notelp'] == b["phone_number"]:
+        current_app.logger.debug("masuk ke if")
+        status = "1"
+    else:
+        current_app.logger.debug("else mas")
+        status = "0"
+    return status
+    
+def sendchat(telp, from_user, message, created_at):
+    collection = get_collection("chat")
+    current_app.logger.debug("Manggil function")
+    a = number_match(telp)
+    if a == '1':
+        result = collection.insert_one({"telp" : telp, "from_user" : from_user, "message" : message, "sent" : created_at, "status" : True})
+    elif a == '0':
+        result = collection.insert_one({"telp" : telp, "from_user" : from_user, "message" : message, "sent" : created_at, "status" : False})  
+    return result.inserted_id
+
+def update_chats(id, chat):
+    # param 1 > chat_old , param 2 > chat_new
+    collection = get_collection("chat")
+    current_app.logger.debug(id)
+    current_app.logger.debug(chat)
+    result = collection.update_one({"_id": ObjectId(id)},  { "$set": chat }, upsert=False)
+    return ObjectId(id)
+
+def delete_chat(id):
+    collection = get_collection("chat")
+    result = collection.delete_one({"_id": ObjectId(id)})
+    return ObjectId(id)
+
+def getbyID(id):
+    collection = get_collection("chat")
+    result = collection.find_one({"_id" : ObjectId(id)})
+    return collection.find_one(result)
+
+def get_byDate(chat):
+    collection = get_collection("chat")
+    result = collection.insert_one(chat)
+    return result.inserted_id
+
+def get_byAll():
+    collection = get_collection("chat")
+    result = collection.find({}, {"_id" : 0})
+    return list(result)
+
+def get_byNick(nick):
+    collection = get_collection("chat")
+    data = idmatch(nick)
+    return list(data)
+
+#route yang melisting message by channel id
+def get_channel_messages(id):
+    collection = get_collection("chat")
+    result = collection.find({"to_user" : id}, {"_id" : 0})
+    return result
+
+#buat web api untuk listing chat by (a) user_from dan user_to, (b) baris message dan user_to
+def get_personalChat_id(reciever, sender):
+    collection = get_collection('chat')
+    result = collection.find({"to_user" : reciever, "from_user" : sender}, {"_id" : 0})
+    return result
+
+def get_personalChat_text(reciever, text):
+    collection = get_collection('chat')
+    result = collection.find({"to_user" : reciever, "message" : text}, {"_id" : 0, "from_user" : 0})
+    return result
